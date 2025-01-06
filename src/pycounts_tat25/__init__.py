c4801c6e423617defca8b9c06e12f54b8c4655bb
Tiffany Timbers <tiffany.timbers@gmail.com>
2025-01-06 12:59:20 -0800

    setup package

--- src/pycounts_tat25/__init__.py
+++ src/pycounts_tat25/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("pycounts_tat25")
+
+
+from pycounts_tat25.pycounts_tat25 import count_words
+from pycounts_tat25.plotting import plot_words
